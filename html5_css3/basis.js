eb5f3cbe97e513f7661e86c6d871c6a246e74e38
Wxw <wangxu5277@163.com>
2020-12-02 16:31:47 +0800

    updata

---
+++ html5_css3/basis.js
@@ -0,0 +1,7 @@
+//基础知识
+
+//浏览器：渲染网面给用户查看、让用户通过浏览器与网页交互；IE(Trident) Chrome(WebKit/Blink) Firefox(Gecko) safari(WebKit) Opera(Presto)
+
+//服务器（高配置电脑）：存储数据
+
+//浏览器渲染页面原理：浏览器获取网页上的内容信息，并缓存于Internet Cache 文件夹，然后再渲染出来呈现给用户查看
